05be9e86b1b0dc92fbe9848ce13c7588340ecd60
nic0michael <nicomichael2012@gmail.com>
2026-09-15 15:53:56 +0200

    swagger runs

---
+++ backend/api/README.md
@@ -0,0 +1,10 @@
+
+## To launch this API
+```bash
+# Run this command:
+./gradlew :backend:api:bootRun
+
+```
+
+## Swagger can be viewed here
+**[http://localhost:8080/swagger-ui/index.html](http://localhost:8080/swagger-ui/index.html)
--- backend/api/src/main/java/com/nm/tranproc/api/producer/ProducerImpl.java
+++ backend/api/src/main/java/com/nm/tranproc/api/producer/ProducerImpl.java
@@ -1,7 +1,14 @@
 package com.nm.tranproc.api.producer;
+import com.nm.tranproc.api.exception.TransactionServiceException;
+import com.nm.tranproc.api.request.Request;
+import com.nm.tranproc.api.response.ResponseDTO;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ProducerImpl {
+public class ProducerImpl implements Producer {
 
+  @Override
+  public ResponseDTO sendTransaction(Request request) throws TransactionServiceException {
+    return null;
+  }
 }
